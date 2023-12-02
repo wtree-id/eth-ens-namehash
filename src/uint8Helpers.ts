d1a1cd8b12daef2543623b96970fa893eb0545a9
Mikhail <16622558+mmv08@users.noreply.github.com>
2023-12-02 15:25:14 +0100

    Linting

--- src/uint8Helpers.ts
+++ src/uint8Helpers.ts
@@ -1,15 +1,15 @@
 // The code is taken from:
 // https://github.com/sindresorhus/uint8array-extras/
-const byteToHexLookupTable = Array.from({length: 256}, (_, index) => index.toString(16).padStart(2, '0'));
+const byteToHexLookupTable = Array.from({ length: 256 }, (_, index) => index.toString(16).padStart(2, '0'));
 
 function uint8ArrayToHex(array: Uint8Array): string {
-    let hexString = '';
+  let hexString = '';
 
-    for (let index = 0; index < array.length; index++) {
-        hexString += byteToHexLookupTable[array[index]];
-    }
+  for (let index = 0; index < array.length; index++) {
+    hexString += byteToHexLookupTable[array[index]];
+  }
 
-    return hexString;
+  return hexString;
 }
 
-export {uint8ArrayToHex};
+export { uint8ArrayToHex };
--- test/namehash.test.ts
+++ test/namehash.test.ts
@@ -1,62 +1,61 @@
-import { hash, normalize } from '../src'
+import { hash, normalize } from '../src';
 
 // Test results specified in original ENS Proposal:
 // https://github.com/ethereum/EIPs/issues/137
 
 test('empty name', () => {
-  const input = ''
-  const expected = '0x0000000000000000000000000000000000000000000000000000000000000000'
-  const output = hash(input)
+  const input = '';
+  const expected = '0x0000000000000000000000000000000000000000000000000000000000000000';
+  const output = hash(input);
 
-  expect(output).toBe(expected)
-})
+  expect(output).toBe(expected);
+});
 
 test('empty param', () => {
-  const expected = '0x0000000000000000000000000000000000000000000000000000000000000000'
-  const output = hash('')
+  const expected = '0x0000000000000000000000000000000000000000000000000000000000000000';
+  const output = hash('');
 
-  expect(output).toBe(expected)
-})
+  expect(output).toBe(expected);
+});
 
 test('TLD eth', () => {
-  const input = 'eth'
-  const expected = '0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae'
-  const output = hash(input)
-  expect(output).toBe(expected)
-})
+  const input = 'eth';
+  const expected = '0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae';
+  const output = hash(input);
+  expect(output).toBe(expected);
+});
 
 test('foo.eth', () => {
-  const input = 'foo.eth'
-  const expected = '0xde9b09fd7c5f901e23a3f19fecc54828e9c848539801e86591bd9801b019f84f'
-  const output = hash(input)
-  expect(output).toBe(expected)
-})
+  const input = 'foo.eth';
+  const expected = '0xde9b09fd7c5f901e23a3f19fecc54828e9c848539801e86591bd9801b019f84f';
+  const output = hash(input);
+  expect(output).toBe(expected);
+});
 
 test('normalize ascii domain', () => {
-  const input = 'foo.eth' // latin chars only
-  const expected = 'foo.eth'
-  const output = normalize(input)
-  expect(output).toBe(expected)
-})
-
+  const input = 'foo.eth'; // latin chars only
+  const expected = 'foo.eth';
+  const output = normalize(input);
+  expect(output).toBe(expected);
+});
 
 test('normalize international domain', () => {
-  const input = 'fоо.eth' // with cyrillic 'o'
-  const expected = 'fоо.eth'
-  const output = normalize(input)
-  expect(output).toBe(expected)
-})
+  const input = 'fоо.eth'; // with cyrillic 'o'
+  const expected = 'fоо.eth';
+  const output = normalize(input);
+  expect(output).toBe(expected);
+});
 
 test('normalize capitalized domain', () => {
-  const input = 'Foo.eth' // latin chars only
-  const expected = 'foo.eth'
-  const output = normalize(input)
-  expect(output).toBe(expected)
-})
+  const input = 'Foo.eth'; // latin chars only
+  const expected = 'foo.eth';
+  const output = normalize(input);
+  expect(output).toBe(expected);
+});
 
 test('normalize emoji domain', () => {
-  const input = '🦚.eth'
-  const expected = '🦚.eth'
-  const output = normalize(input)
-  expect(output).toBe(expected)
-})
+  const input = '🦚.eth';
+  const expected = '🦚.eth';
+  const output = normalize(input);
+  expect(output).toBe(expected);
+});
